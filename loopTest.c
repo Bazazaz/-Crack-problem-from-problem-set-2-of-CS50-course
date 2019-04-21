466918beeb247bdc4948ad627b58cd98dc8dbb24
Vasiliy <x-9@ya.ru>
2019-04-21 05:21:07 +0000

    scaled to 3 chars

--- loopTest.c
+++ loopTest.c
@@ -8,29 +8,44 @@ int main(void)
 {
     char fourChars[5];
 
-    for (char a = 'a'; a <= 'd'; a++)
+    for (char a = 'a'; a <= 'z'; a++)
     {
         fourChars[0] = a;
         printf("%s\n", fourChars);
         fourChars[0] = toupper(a);
         printf("%s\n", fourChars);
-        for (char inta = '0'; inta <= '3'; inta++)
+        for (char inta = '0'; inta <= '9'; inta++)
         {
             fourChars[0] = inta;
             printf("%s\n", fourChars);
         }
-        for (char b = 'a'; b <= 'd'; b++)
+        for (char b = 'a'; b <= 'z'; b++)
         {
             fourChars[1] = b;
             printf("%s\n", fourChars);
             fourChars[1] = toupper(b);
             printf("%s\n", fourChars);
-            for (char intb = '0'; intb <= '3'; intb++)
+            for (char intb = '0'; intb <= '9'; intb++)
             {
                 fourChars[1] = intb;
                 printf("%s\n", fourChars);
             }
+                for (char c = 'a'; c <= 'z'; c++)
+                {
+                    fourChars[2] = c;
+                    printf("%s\n", fourChars);
+                    fourChars[2] = toupper(c);
+                    printf("%s\n", fourChars);
+                    for (char intc = '0'; intc <= '9'; intc++)
+                    {
+                        fourChars[2] = intc;
+                        printf("%s\n", fourChars);
+                    }
+
+                }
+
         }
+
     }
 
 }
