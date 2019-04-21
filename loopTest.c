24d7e997634c4005794bf929e2678b63d6e89ff5
Vasiliy <x-9@ya.ru>
2019-04-21 05:12:11 +0000

    made a scalable loop

--- loopTest.c
+++ loopTest.c
@@ -2,26 +2,35 @@
 #include <unistd.h>
 #include <cs50.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(void)
 {
-    char twoChars[4];
+    char fourChars[5];
 
-    for (char a = 'a'; a <= 'z'; a++)
+    for (char a = 'a'; a <= 'd'; a++)
     {
-        twoChars[0] = a;
-        // printf("%s\n", twoChars);
-        for (char b = 'a'; b <= 'z'; b++)
+        fourChars[0] = a;
+        printf("%s\n", fourChars);
+        fourChars[0] = toupper(a);
+        printf("%s\n", fourChars);
+        for (char inta = '0'; inta <= '3'; inta++)
         {
-            twoChars[1] = b;
-            printf("%s\n", twoChars);
-            // for (char c = 'a'; c <= 'z'; c++)
-            // {
-            //     twoChars[2] = c;
-            //     printf("%s\n", twoChars);
-            // }
+            fourChars[0] = inta;
+            printf("%s\n", fourChars);
+        }
+        for (char b = 'a'; b <= 'd'; b++)
+        {
+            fourChars[1] = b;
+            printf("%s\n", fourChars);
+            fourChars[1] = toupper(b);
+            printf("%s\n", fourChars);
+            for (char intb = '0'; intb <= '3'; intb++)
+            {
+                fourChars[1] = intb;
+                printf("%s\n", fourChars);
+            }
         }
     }
-
 
 }
