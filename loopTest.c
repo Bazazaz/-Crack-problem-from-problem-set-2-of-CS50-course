dea2a3afc50a4bbbbe0c31bb4b80195a3a3b0c8a
Vasiliy <x-9@ya.ru>
2019-04-22 09:48:23 +0000

    first commit with ref array generator

--- loopTest.c
+++ loopTest.c
@@ -6,51 +6,28 @@
 
 int main(void)
 {
-    char fourChars[5];
-
     // open output file
     FILE *f = fopen("output.txt", "w");
 
-    for (char a = 'a'; a <= 'c'; a++)
+    //reference array
+    char refArray[62];
+
+    //array creation loop
+    int counter = 0;
+    for(char a = 'a'; a <= 'z'; a++)
     {
-        fourChars[0] = a;
-        fprintf (f, "%s\n", fourChars);
-        fourChars[0] = toupper(a);
-        fprintf (f, "%s\n", fourChars);
-        for (char inta = '0'; inta <= '3'; inta++)
-        {
-            fourChars[0] = inta;
-            fprintf (f, "%s\n", fourChars);
-        }
-        fourChars[0] = a;
-        for (char b = 'a'; b <= 'c'; b++)
-        {
-            fourChars[1] = b;
-            fprintf (f, "%s\n", fourChars);
-            fourChars[1] = toupper(b);
-            fprintf (f, "%s\n", fourChars);
-            for (char intb = '0'; intb <= '3'; intb++)
-            {
-                fourChars[1] = intb;
-                fprintf (f, "%s\n", fourChars);
-            }
-                // for (char c = 'a'; c <= 'z'; c++)
-                // {
-                //     fourChars[2] = c;
-                //     fprintf (f, "%s\n", fourChars);
-                //     fourChars[2] = toupper(c);
-                //     fprintf (f, "%s\n", fourChars);
-                //     for (char intc = '0'; intc <= '9'; intc++)
-                //     {
-                //         fourChars[2] = intc;
-                //         fprintf (f, "%s\n", fourChars);
-                //     }
-
-                // }
-
-        }
-
+        refArray[counter] = a;
+        counter++;
+        refArray[counter] = toupper(a);
+        counter++;
     }
+    for(int b = 0; b <= 9; b++)
+    {
+        refArray[counter] = b;
+        counter++;
+    }
+
+    fprintf (f, "%s\n", refArray);
     fclose (f);
 
 }
