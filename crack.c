50bc3c5bfa5e23de5e5588aa34e2174d9d135455
Vasiliy <x-9@ya.ru>
2019-05-02 09:04:34 +0000

    5 char working version

--- crack.c
+++ crack.c
@@ -3,16 +3,16 @@
 #include <unistd.h>
 #include <cs50.h>
 #include <string.h>
+#include <ctype.h>
 
-int checkForMatch()//1.takes chars array, given hash, salt. 2.Generates new hash from array and salt 3.Compares two hashes
-                   //3.1 if no match - do nothing 3.2 if match - return mached array
+int compareHashes(char a[], char b[]);
 
 int main(int argc, string argv[])
 {
     if (argc == 2)
     {
         string hash = argv[1];
-        printf("hash is: %s\n", hash);
+        // printf("hash is: %s\n", hash);
 
         //get salt from hash
         char salt[3];
@@ -23,14 +23,12 @@ int main(int argc, string argv[])
             sL++;
         }
         salt[2] = '\0';
-        printf("salt is: %s\n", salt);
-        printf("\n");
+        // printf("salt is: %s\n", salt);
+        // printf("\n");
 
         //generate ref array -------------------------------------------------
         char refArray[53];
         int counter = 0;
-
-        //chars part
         for(char a = 'a'; a <= 'z'; a++)
         {
             refArray[counter] = a;
@@ -38,14 +36,6 @@ int main(int argc, string argv[])
             refArray[counter] = toupper(a);
             counter++;
         }
-
-        //integers part
-        // for(char b = '0'; b <= '9'; b++)
-        // {
-        //     refArray[counter] = b;
-        //     counter++;
-        // }
-        //---------------------------------------------------------------------------
 
         // chars arrays generetion --------------------------------------------------
         char tempArray1[2] = "0";
@@ -66,25 +56,50 @@ int main(int argc, string argv[])
         for(int d = 0; d < 52; d++)
         {
             tempArray1[0] = refArray[d];
-            print(ff, tempArray1);
+            string currentHash = crypt(tempArray1, salt);
+            if(compareHashes(currentHash, hash) == 0)
+            {
+                printf("%s\n", tempArray1);
+                return 0;
+                break;
+            }
             for(int e = 0; e < 52; e++)
             {
                 tempArray2[0] = tempArray1[0];
                 tempArray2[1] = refArray[e];
-                print(ff, tempArray2);
+                currentHash = crypt(tempArray2, salt);
+                if(compareHashes(currentHash, hash) == 0)
+                {
+                    printf("%s\n", tempArray2);
+                    return 0;
+                    break;
+                }
                 for(int f = 0; f < 52; f++)
                 {
                     tempArray3[0] = tempArray1[0];
                     tempArray3[1] = tempArray2[1];
                     tempArray3[2] = refArray[f];
-                    print(ff, tempArray3);
+
+                    currentHash = crypt(tempArray3, salt);
+                    if(compareHashes(currentHash, hash) == 0)
+                    {
+                        printf("%s\n", tempArray3);
+                        return 0;
+                        break;
+                    }
                     for(int g = 0; g < 52; g++)
                     {
                         tempArray4[0] = tempArray1[0];
                         tempArray4[1] = tempArray2[1];
                         tempArray4[2] = tempArray3[2];
                         tempArray4[3] = refArray[g];
-                        print(ff, tempArray4);
+                        currentHash = crypt(tempArray4, salt);
+                        if(compareHashes(currentHash, hash) == 0)
+                        {
+                            printf("%s\n", tempArray4);
+                            return 0;
+                            break;
+                        }
                         for(int h = 0; h < 52; h++)
                         {
                             tempArray5[0] = tempArray1[0];
@@ -92,16 +107,39 @@ int main(int argc, string argv[])
                             tempArray5[2] = tempArray3[2];
                             tempArray5[3] = tempArray4[3];
                             tempArray5[4] = refArray[h];
-                            print(ff, tempArray5);
+                            currentHash = crypt(tempArray5, salt);
+                            if(compareHashes(currentHash, hash) == 0)
+                            {
+                                printf("%s\n", tempArray5);
+                                return 0;
+                                break;
+                            }
                         }
                     }
                 }
             }
         }
+
     }
     else
     {
         printf("ERROR\n");
         return 1;
     }
+}
+
+int compareHashes(char a[], char b[])
+{
+   int c = 0;
+
+   while (a[c] == b[c]) {
+      if (a[c] == '\0' || b[c] == '\0')
+         break;
+      c++;
+   }
+
+   if (a[c] == '\0' && b[c] == '\0')
+      return 0;
+   else
+      return -1;
 }
